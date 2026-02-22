36a5022e8ccd8653aee69a761ebbb99c9caea682
wylwswswws <127329830+wylwswswws@users.noreply.github.com>
2026-02-22 00:33:57 +0000

    Deploying to gh-pages from @ wylwswswws/wylwswswws.github.io@0b33f9e5bae77f004e4e9a0fac9064ae3f377253 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,28 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("mailto:%79%75%6C%75%6E%77%75%30%32@%31%36%33.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-custom_social',
